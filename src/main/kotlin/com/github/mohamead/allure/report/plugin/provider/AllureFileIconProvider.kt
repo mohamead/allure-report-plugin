5e8bc927bbd707865e1e7556a9ec151bd2089562
mohamead <mohammed.adrik@gmail.com>
2025-11-17 18:18:03 +0100

    Add IconProvider for io.qameta.allure

---
+++ src/main/kotlin/com/github/mohamead/allure/report/plugin/provider/AllureFileIconProvider.kt
@@ -0,0 +1,20 @@
+package com.github.mohamead.allure.report.plugin.provider
+
+import com.github.mohamead.allure.report.plugin.action.MainActionGroup.Companion.allFolders
+import com.github.mohamead.allure.report.plugin.icon.PluginIcons.allure
+import com.github.mohamead.allure.report.plugin.util.matchAny
+import com.intellij.ide.FileIconProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import javax.swing.Icon
+
+internal class AllureFileIconProvider : FileIconProvider {
+
+    override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
+        if (file.isDirectory && allFolders.matchAny(file.name.lowercase().trim())) {
+            return allure
+        }
+        return null
+    }
+
+}
